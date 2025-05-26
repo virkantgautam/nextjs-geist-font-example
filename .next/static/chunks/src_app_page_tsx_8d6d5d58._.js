c328d1975a3ce5e55125b9416a0fd616145efcbd
virkantgautam <vikkygautam999@gmail.com>
2025-05-26 23:00:16 +0530

    Add .next/static/chunks/src_app_page_tsx_8d6d5d58._.js 1748280615900

---
+++ .next/static/chunks/src_app_page_tsx_8d6d5d58._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/_767f9dfe._.js"
+],
+    source: "dynamic"
+});
